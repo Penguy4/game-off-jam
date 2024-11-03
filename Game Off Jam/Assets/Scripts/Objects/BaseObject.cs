780291ccbe8e9dc12bada858be7d5acc955390e3
Penguy <72886631+Penguy4@users.noreply.github.com>
2024-11-03 08:59:14 -0800

    Object Scriptable Object

---
+++ Game Off Jam/Assets/Scripts/Objects/BaseObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ObjectData", menuName = "ScriptableObjects/New Object", order = 1)]
+public class BaseObject : ScriptableObject
+{
+    [Header("Basic Info")]
+    public string name = "";
+    public Sprite sprite;
+    [Header("Interaction Info")]
+    public bool isEvidence;
+    public GameObject objectNeededToClean;
+    [Header("Spawning Info")]
+    public bool randomized;
+    public GameObject[] randomSpawnLocations;
+
+
+}
